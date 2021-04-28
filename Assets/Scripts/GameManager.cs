369b0e08617ce6ca7db855a168a20f893c38f522
Gerard Cuello <fontinixxl@users.noreply.github.com>
2021-04-28 20:05:18 +0200

    Import PlayFab and implement silent auth
    Add PlayFabAuthService with silent auth method to login using
    CustomId.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,78 @@
+﻿using PlayFab;
+using PlayFab.ClientModels;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    // Meta references to panels we need to show / hide
+    public Text StatusText;
+
+    private PlayFabAuthService _AuthService;
+    
+    // Settings for what data to get from playfab on login
+    public GetPlayerCombinedInfoRequestParams InfoRequestParams;
+
+    private void Awake()
+    {
+        _AuthService = PlayFabAuthService.Instance;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayFabAuthService.OnLoginSuccess += OnLoginSuccess;
+        PlayFabAuthService.OnPlayFabError += OnPlayFaberror;
+
+        // Set the data we want at login from what we chose in our meta data.
+        _AuthService.InfoRequestParams = InfoRequestParams;
+        // Start the authentication process.
+        _AuthService.Authenticate();
+    }
+
+    /// <summary>
+    /// Login Successfully - Goes to next screen.
+    /// </summary>
+    /// <param name="result"></param>
+    private void OnLoginSuccess(PlayFab.ClientModels.LoginResult result)
+    {
+        Debug.LogFormat("Logged In as: {0}", result.PlayFabId);
+        StatusText.text = "Logged In as: " + result.PlayFabId.ToString();
+        //LoginPanel.SetActive(false);
+        //LoggedinPanel.SetActive(true);
+        //UserName.text = result.InfoResultPayload.AccountInfo.Username ?? result.PlayFabId;
+    }
+
+    /// <summary>
+    /// Error handling for when Login returns errors.
+    /// </summary>
+    /// <param name="error"></param>
+    private void OnPlayFaberror(PlayFabError error)
+    {
+        //Basic error cases on Login
+        switch (error.Error)
+        {
+            case PlayFabErrorCode.InvalidEmailAddress:
+            case PlayFabErrorCode.InvalidPassword:
+            case PlayFabErrorCode.InvalidEmailOrPassword:
+                StatusText.text = "Invalid Email or Password";
+                break;
+
+            case PlayFabErrorCode.AccountNotFound:
+                StatusText.text = "Account Not Found";
+                //RegisterPanel.SetActive(true);
+                //SigninPanel.SetActive(false);
+                return;
+            default:
+                StatusText.text = error.GenerateErrorReport();
+                break;
+        }
+
+        //Report to debug console
+        Debug.Log(error.Error);
+        Debug.LogError(error.GenerateErrorReport());
+    }
+
+}
---
+++ Assets/Scripts/PlayFab/PlayFabAuthService.cs
@@ -0,0 +1,135 @@
+﻿using UnityEngine;
+using PlayFab;
+using PlayFab.ClientModels;
+using System;
+using LoginResult = PlayFab.ClientModels.LoginResult;
+
+public enum Authtypes
+{
+    None,
+    Silent,
+    //UsernameAndPassword,
+}
+
+public class PlayFabAuthService
+{
+    // Events to subscribe for this service
+    public delegate void LoginSuccessEvent(LoginResult success);
+    public static event LoginSuccessEvent OnLoginSuccess;
+    
+    public delegate void PlayFabErrorEvent(PlayFabError error);
+    public static event PlayFabErrorEvent OnPlayFabError;
+
+    // These are fields that we set when we are using the service.
+    public string AuthTicket;
+    public GetPlayerCombinedInfoRequestParams InfoRequestParams;
+
+    // Accessbility for PlayFab ID & Session Tickets
+    public static string PlayFabId { get { return _playFabId; } }
+    private static string _playFabId;
+    public static string SessionTicket { get { return _sessionTicket; } }
+    private static string _sessionTicket;
+
+    private const string _PlayFabAuthTypeKey = "PlayFabAuthType";
+
+    public static PlayFabAuthService Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = new PlayFabAuthService();
+            }
+            return _instance;
+        }
+    }
+    private static PlayFabAuthService _instance;
+
+    public PlayFabAuthService()
+    {
+        _instance = this;
+    }
+
+    /// <summary>
+    /// Remember the type of authenticate for the user
+    /// </summary>
+    public Authtypes AuthType
+    {
+        get
+        {
+            return (Authtypes)PlayerPrefs.GetInt(_PlayFabAuthTypeKey, 0);
+        }
+        set
+        {
+            PlayerPrefs.SetInt(_PlayFabAuthTypeKey, (int)value);
+        }
+    }
+
+    /// <summary>
+    /// Authenticate the user by the Auth Type that was defined.
+    /// </summary>
+    public void Authenticate(Authtypes authType)
+    {
+        AuthType = authType;
+        Authenticate();
+    }
+
+    /// <summary>
+    /// Authenticate the user by the Auth Type that was defined.
+    /// </summary>
+    public void Authenticate()
+    {
+        Debug.LogFormat("Authenticating with {0}", AuthType.ToString());
+        switch (AuthType)
+        {
+            case Authtypes.None:
+                // If there is no Authtype we want to loggin silently anyway
+                Authenticate(Authtypes.Silent);
+                break;
+            case Authtypes.Silent:
+                SilentlyAuthenticate();
+                break;
+        }
+    }
+
+    private void SilentlyAuthenticate(System.Action<LoginResult> callback = null)
+    {
+        PlayFabClientAPI.LoginWithCustomID(new LoginWithCustomIDRequest()
+        {
+            TitleId = PlayFabSettings.TitleId,
+            CustomId = SystemInfo.deviceUniqueIdentifier,
+            CreateAccount = true,
+            InfoRequestParameters = InfoRequestParams
+        }, (result) => {
+            //Store Identity and session
+            _playFabId = result.PlayFabId;
+            _sessionTicket = result.SessionTicket;
+
+            //check if we want to get this callback directly or send to event subscribers.
+            if (callback == null && OnLoginSuccess != null)
+            {
+                //report login result back to the subscriber
+                OnLoginSuccess?.Invoke(result);
+            }
+            else if (callback != null)
+            {
+                //report login result back to the caller
+                callback?.Invoke(result);
+            }
+        }, (error) => {
+            //report errro back to the subscriber
+            if (callback == null && OnPlayFabError != null)
+            {
+                OnPlayFabError.Invoke(error);
+            }
+            else
+            {
+                //make sure the loop completes, callback with null
+                callback?.Invoke(null);
+                //Output what went wrong to the console.
+                Debug.LogError(error.GenerateErrorReport());
+            }
+
+        });
+    }
+}
