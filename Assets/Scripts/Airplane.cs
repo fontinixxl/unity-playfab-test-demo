0201cfea38cddb52032d220766ed847adcca7f72
Gerard Cuello <fontinixxl@users.noreply.github.com>
2021-04-29 16:30:39 +0200

    Implement Abstract class spawnable

--- Assets/Scripts/Airplane.cs
+++ Assets/Scripts/Airplane.cs
@@ -1,31 +1,10 @@
 ﻿using UnityEngine;
-using System.Collections;
 
-public class Airplane : MonoBehaviour {
-	// Use this for initialization
-	void Start () {
+public class Airplane : Spawnable {
 
-	}
-
-	// Update is called once per frame
-	void Update () {
-
-		// if airplane goes past the left edge, destroy it
-		if (transform.position.x < -25) {
-			Destroy(gameObject);
-		}
-		else {
-
-			// twice as fast as the skyscraper spawner speed toward the left
-			transform.Translate(-SkyscraperSpawner.speed * 2 * Time.deltaTime, 0, 0, Space.World);
-		}
-	}
-
-	void OnTriggerEnter(Collider other) {
-
-		// trigger helicopter's explode function via HeliController component
-		// and then destroy this airplane as well
-		//other.transform.parent.gameObject.GetComponent<HeliController>().Explode();
-		//Destroy(gameObject);
+    protected override void Scroll()
+    {
+		// twice as fast as the skyscraper spawner speed toward the left
+		transform.Translate(-SkyscraperSpawner.speed * 2 * Time.deltaTime, 0, 0, Space.World);
 	}
 }
--- Assets/Scripts/Skyscraper.cs
+++ Assets/Scripts/Skyscraper.cs
@@ -1,30 +1,9 @@
 ﻿using UnityEngine;
-using System.Collections;
 
-public class Skyscraper : MonoBehaviour {
+public class Skyscraper : Spawnable {
 
-	// Use this for initialization
-	void Start () {
-
-	}
-
-	// Update is called once per frame
-	void Update () {
-
-		// despawn when past left edge of the screen (camera)
-		if (transform.position.x < -25) {
-			Destroy(gameObject);
-		}
-		else {
-
-			// scroll based on SkyscraperSpawner static variable, speed
-			transform.Translate(-SkyscraperSpawner.speed * Time.deltaTime, 0, 0);
-		}
-	}
-
-	void OnTriggerEnter(Collider other) {
-
-		// trigger helicopter to explode when it collides with this
-		//other.transform.parent.gameObject.GetComponent<HeliController>().Explode();
-	}
+    protected override void Scroll()
+    {
+		transform.Translate(-SkyscraperSpawner.speed * Time.deltaTime, 0, 0);
+    }
 }
