40757b7d2a318bea9739ab917fa98668088936af
Gerard Cuello <fontinixxl@users.noreply.github.com>
2021-05-04 10:44:36 +0200

    Add cloudscript statistics

--- PlayFab/CloudScript.js
+++ PlayFab/CloudScript.js
@@ -25,5 +25,44 @@ function SubtractLife() {
     return server.SubtractUserVirtualCurrency(subtractVcRequest);
 }
 
+UpdateStatistic = function (args, context) {
+  var event = context.playStreamEvent;
+
+  if(event != null) {
+    var request = {
+      PlayFabId: currentPlayerId,
+      Statistics: [{
+        StatisticName: event.stat_name,
+        Value: event.value
+      }]
+    };
+
+    server.UpdatePlayerStatistics(request);
+  }
+};
+
+UpdateStatistics = function (args, context) {
+  var event = context.playStreamEvent;
+
+  if(event != null) {
+    var stats = event.stats;
+    var statArray = [];
+
+    for(var key in stats) {
+        var value = stats[key];
+        statArray.push({StatisticName: key, Value: value});
+	}
+
+    var request = {
+      PlayFabId: currentPlayerId,
+      Statistics: statArray
+    };
+
+    server.UpdatePlayerStatistics(request);
+  }
+};
+
 ///////////////////////// Define the handlers /////////////////////////
 handlers.SaveProgress = SaveProgress;
+handlers.UpdateStatistic = UpdateStatistic;
+handlers.UpdateStatistics = UpdateStatistics;
