706d6ac6f6e8ca405c680b8bafa6a2149a9e2fe6
Gerard Cuello <fontinixxl@users.noreply.github.com>
2021-05-04 23:09:29 +0200

    Fix bug with coin rotating frame-independent

--- Assets/Scripts/Game/Spawner/Objects/Coin.cs
+++ Assets/Scripts/Game/Spawner/Objects/Coin.cs
@@ -4,14 +4,14 @@ using UnityEngine.UI;
 
 public class Coin : MonoBehaviour
 {
-    public float rotationSpeed = 3f;
+    public float rotationSpeed = 200f;
 
     private void Update()
     {
         transform.Translate(-Spawner.Speed * Time.deltaTime, 0, 0, Space.World);
 
         // infinitely rotate this coin about the Y axis in world space
-        transform.Rotate(0, rotationSpeed, 0, Space.World);
+        transform.Rotate(0, rotationSpeed * Time.deltaTime, 0, Space.World);
     }
 
 }
