6a1ed379105531395bcf2df3cd623dfdd41288c0
Gerard Cuello <fontinixxl@users.noreply.github.com>
2021-05-04 10:37:56 +0200

    Update cloudsript

--- PlayFab/CloudScript.js
+++ PlayFab/CloudScript.js
@@ -1,6 +1,12 @@
-var COINS_CURRENCY_CODE = "SC";
+var COINS_CURRENCY_CODE = "CO";
+var LIVES_CURRENCY_CODE = "LV";
+
 ///////////////////////// Cloud Script Handler Functions /////////////////////////
 function SaveProgress(args) {
+
+    if (args.IsGameOver){
+        SubtractLife();
+    }
     // set up Coins VC
     var addVcRequest = {
         PlayFabId: currentPlayerId,
@@ -8,6 +14,15 @@ function SaveProgress(args) {
         Amount: args.CoinsCollected
     };
     return server.AddUserVirtualCurrency(addVcRequest);
+}
+
+function SubtractLife() {
+    var subtractVcRequest = {
+        PlayFabId: currentPlayerId,
+        VirtualCurrency: LIVES_CURRENCY_CODE,
+        Amount: 1
+    };
+    return server.SubtractUserVirtualCurrency(subtractVcRequest);
 }
 
 ///////////////////////// Define the handlers /////////////////////////
